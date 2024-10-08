f026d78c32677462e8a97ac2a59498cb14bdccb3
majinbru <majinbrum@gmail.com>
2024-10-08 21:22:17 +0200

    fix project node modules, packages, etc with clearing cache

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function MenuStack() {
 	return (
-		<Stack>
+		<Stack screenOptions={{}}>
 			{/* primo metodo per cambiare titolo dello screen*/}
 			<Stack.Screen name='index' options={{ title: "Menu" }} />
 		</Stack>
--- src/components/CartListItem.tsx
+++ src/components/CartListItem.tsx
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import React from "react";
 import Colors from "../constants/Colors";
 import { CartItem } from "../types";
-import { Link } from "expo-router";
+import Link from "expo-router";
 import { defaultPizzaImage } from "./ProductListItem";
 import { FontAwesome } from "@expo/vector-icons";
 import { useCart } from "../providers/CartProvider";
