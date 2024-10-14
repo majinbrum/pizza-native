a02d4d0e6fe9a80e74242605d466fedb02447b28
majinbru <majinbrum@gmail.com>
2024-10-14 13:06:52 +0200

    18. Supabase Typescript

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, Image, Pressable } from "react-native";
 import Colors from "@/src/constants/Colors";
-import { Product } from "../types";
+import { Tables } from "../types";
 import { Href, Link, useSegments } from "expo-router";
 
 export const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 type ProductListItemProps = {
-	product: Product;
+	// product: Product; // now that we have the database types, we don't have to use the manually written types:
+	product: Tables<"products">;
 };
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
@@ -14,9 +15,15 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
 	const path: Href = `/${segments[0]}/menu/${product.id}` as Href;
 
 	return (
-		<Link href={path} asChild>
+		<Link
+			href={path}
+			asChild>
 			<Pressable style={styles.container}>
-				<Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode='contain' />
+				<Image
+					source={{ uri: product.image || defaultPizzaImage }}
+					style={styles.image}
+					resizeMode='contain'
+				/>
 				<Text style={styles.title}>{product.name}</Text>
 				<Text style={styles.price}>${product.price}</Text>
 			</Pressable>
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../database.types";
 
 const supabaseUrl = "https://lhcmeejhamxllisjizjf.supabase.co";
 // anon public is safe to use in the browser because we will use auth from the user
 const supabaseAnonKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoY21lZWpoYW14bGxpc2ppempmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2ODM3NTEsImV4cCI6MjA0NDI1OTc1MX0.xBLwkJa231MyEslxTptXDEG-BwPTsFYaGs4Mp-I7H5g";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+// adding the type <Database> the data fetched in the api/products/index (and wherever they're used) will already be typed automatically
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
 	auth: {
 		storage: AsyncStorage,
 		autoRefreshToken: true,
