6c65fd119f9d8c31e5b12f810f0f810bb4c413c3
majinbru <majinbrum@gmail.com>
2024-10-15 20:03:04 +0200

    25. Stripe + React Native

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,6 +11,7 @@ import { useColorScheme } from "@/src/components/useColorScheme";
 import CartProvider from "../providers/CartProvider";
 import AuthProvider from "../providers/AuthProvider";
 import QueryProvider from "../providers/QueryProvider";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -54,30 +55,32 @@ function RootLayoutNav() {
 
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<AuthProvider>
-				<QueryProvider>
-					<CartProvider>
-						<Stack>
-							<Stack.Screen
-								name='(auth)'
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name='(admin)'
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name='(user)'
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name='cart'
-								options={{ presentation: "modal" }}
-							/>
-						</Stack>
-					</CartProvider>
-				</QueryProvider>
-			</AuthProvider>
+			<StripeProvider publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY || ""}>
+				<AuthProvider>
+					<QueryProvider>
+						<CartProvider>
+							<Stack>
+								<Stack.Screen
+									name='(auth)'
+									options={{ headerShown: false }}
+								/>
+								<Stack.Screen
+									name='(admin)'
+									options={{ headerShown: false }}
+								/>
+								<Stack.Screen
+									name='(user)'
+									options={{ headerShown: false }}
+								/>
+								<Stack.Screen
+									name='cart'
+									options={{ presentation: "modal" }}
+								/>
+							</Stack>
+						</CartProvider>
+					</QueryProvider>
+				</AuthProvider>
+			</StripeProvider>
 		</ThemeProvider>
 	);
 }
