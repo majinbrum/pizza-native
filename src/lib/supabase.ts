8b206cc60507a0328ffe93c40b15a4d0761b0cf2
majinbru <majinbrum@gmail.com>
2024-10-15 18:35:19 +0200

    23. Supabase Local Setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,18 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../database.types";
 
-const supabaseUrl = "https://lhcmeejhamxllisjizjf.supabase.co";
+import Constants from "expo-constants";
+
+// Types on this are wrong. This exists, and `host` does not.
+const origin = (Constants?.expoConfig as unknown as { hostUri?: string })?.hostUri?.split(":").shift();
+
+if (!origin) throw new Error("Could not determine origin");
+
+const supabaseUrl = `http://${origin}:54321`;
+
+// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
 // anon public is safe to use in the browser because we will use auth from the user
-const supabaseAnonKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoY21lZWpoYW14bGxpc2ppempmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2ODM3NTEsImV4cCI6MjA0NDI1OTc1MX0.xBLwkJa231MyEslxTptXDEG-BwPTsFYaGs4Mp-I7H5g";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
 
 // adding the type <Database> the data fetched in the api/products/index (and wherever they're used) will already be typed automatically
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
